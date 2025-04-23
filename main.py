75dd0dbfd6cab356c8525655a7b4c5814ad0ed54
jmrkd-studio <108296592+MaheshTheDeveloper@users.noreply.github.com>
2025-04-23 20:22:35 +0530

    --disnake-init

--- main.py
+++ main.py
@@ -1,52 +1,20 @@
-import discord
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-PREFIX = os.getenv('DISCORD_PREFIX')
+from dotenv import load_dotenv
 
-intents = discord.Intents.default()
-intents.message_content = True
+load_dotenv("secrets.env")
+from utils.bot import OGIROID
 
-client = discord.Client(intents=intents)
-
-# Starting
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-# Member Join
-@client.event
-async def on_member_join(member):
-    welcome_channel = discord.utils.get(member.guild.text_channels, name='welcome')
-    if welcome_channel:
-        await welcome_channel.send(f'Welcome {member.mention} to the server!')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    # word moderation
-    if "shit" in message.content.lower():
-        await message.delete()
-        await message.channel.send(f'{message.author.mention}, please do not use that word!')
+bot = OGIROID()
+TOKEN = bot.config.tokens.bot
 
 
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello World! I am alive!')
-    
-    # Help 
-    if message.content.startswith(f'{PREFIX}help'):
-        await message.channel.send('Hello World! I am alive to help!')
-    
-    # Music
-    if message.content.startswith(f'{PREFIX}music'):
-        await message.channel.send('Hello World! I am alive to play music!')
-    
-    # Meme 
-    if message.content.startswith(f'{PREFIX}meme'):
-        await message.channel.send('Hello World! I am alive to make you laugh!')
+def main():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            bot.load_extension(f"cogs.{filename[:-3]}")
+    bot.run(TOKEN)
 
-client.run(TOKEN)
+
+if __name__ == "__main__":
+    main()
