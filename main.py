3d1a91c195b5cdf397987d15c52acb2728a09c9c
jmrkd-studio <108296592+MaheshTheDeveloper@users.noreply.github.com>
2025-04-23 19:52:46 +0530

    --more-commands

--- main.py
+++ main.py
@@ -31,9 +31,22 @@ async def on_message(message):
     # word moderation
     if "shit" in message.content.lower():
         await message.delete()
+        await message.channel.send(f'{message.author.mention}, please do not use that word!')
 
 
     if message.content.startswith(f'{PREFIX}hello'):
+        await message.channel.send('Hello World! I am alive!')
+    
+    # Help 
+    if message.content.startswith(f'{PREFIX}help'):
+        await message.channel.send('Hello World! I am alive!')
+    
+    # Music
+    if message.content.startswith(f'{PREFIX}music'):
+        await message.channel.send('Hello World! I am alive!')
+    
+    # Meme 
+    if message.content.startswith(f'{PREFIX}help'):
         await message.channel.send('Hello World! I am alive!')
 
 client.run(TOKEN)
